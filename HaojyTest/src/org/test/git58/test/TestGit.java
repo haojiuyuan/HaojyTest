69726f26567c928be5b3156de82633fe901598a0
haojiuyuan <haojiuyuan@58ganji.com>
2016-05-06 15:05:23 +0800

    4123

--- HaojyTest/src/org/test/git58/test/TestGit.java
+++ HaojyTest/src/org/test/git58/test/TestGit.java
@@ -1,9 +1,13 @@
-package org.test.git58.test;
+﻿package org.test.git58.test;
 
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("你好");
 		
 		System.out.println("你好");
+		
+		System.out.println("你好1234");
+
+
 	}
 }
